700b04c3ff6a717c162acc75c66d7d43e779bae5
juliale02 <juliale0212@gmail.com>
2022-07-26 08:53:50 -0700

    update AudioMoth1

--- AudioMoth1/app/home/home-page.ts
+++ AudioMoth1/app/home/home-page.ts
@@ -11,15 +11,52 @@ import { EventData } from "tns-core-modules/data/observable"
 import { Button } from "tns-core-modules/ui/button"
 import { TextField } from 'tns-core-modules'
 import { taskDispatcher } from "nativescript-task-dispatcher";
+import { Label } from "tns-core-modules/ui/label";
+import { getViewById } from "tns-core-modules/ui/core/view";
+import { View } from "tns-core-modules/ui/core/view";
+import { alert, AlertOptions } from "tns-core-modules/ui/dialogs";
+
+
 
 export function onNavigatingTo(args: EventData) {
   const page = <Page>args.object
-
+  
   page.bindingContext = new HomeViewModel()
 }
 
+
+
+//var view = require("tns-core-modules/ui/core/view");
+//test function to see if test button works 
+export function test(args: EventData) {
+  const page = <Page>args.object
+
+  //want to check if input values are being obtained correctly? 
+  var inputValue = (<HTMLInputElement>document.getElementById("durMin")).value;
+  
+  //var testLabel = document.getElementById("TestLabel")as HTMLElement | null;
+  var testLabel = (<HTMLInputElement>document.getElementById("TestLabel"));
+  testLabel.setAttribute("TestLabel", inputValue);
+  //testLabel.textContent=inputValue;
+
+}
+
+
+export function onButtonTap(args: EventData) {
+  const button = <Button>args.object;
+  console.log('Button tapped');
+}
+
+
+
+
+
+
+
+
+
 //somehow link emitStartEvent() to the start button in home-page.xml 
-//For now, havent binded the input values, testing mock recording of 2 seconds everyry 2 minutes 
+//For now, havent binded the input values, testing mock recording of 2 seconds every 2 minutes 
 async function emitStartEvent() {
   const isReady = await taskDispatcher.isReady();
   if (!isReady) {
--- AudioMoth1/app/tasks/graph.ts
+++ AudioMoth1/app/tasks/graph.ts
@@ -9,28 +9,11 @@ class DemoTaskGraph implements TaskGraph {
         on: EventListenerGenerator,
         run: RunnableTaskDescriptor
     ): Promise<void> {
-        on("startEvent", run("fastTask"));
-        on("startEvent", run("mediumTask"));
-        on("startEvent", run("slowTask"));
-
         on(
             "startEvent",
-            run("fastTask", { triggeredBy: "schedule" })
-                .every(1, "minutes")
-                .cancelOn("stopEvent")
+            //replace 2 minutes with input 
+            run("record").every(2, "minutes").cancelOn("stopEvent")
         );
-        on(
-            "startEvent",
-            run("mediumTask").every(2, "minutes").cancelOn("stopEvent")
-        );
-        on(
-            "startEvent",
-            run("slowTask").every(4, "minutes").cancelOn("stopEvent")
-        );
-
-        on("slowTaskFinished", run("mediumTask"));
-        on("slowTaskFinished", run("fastTask"));
-        on("mediumTaskFinished", run("fastTask", { triggeredBy: "event" }));
     }
 }
 
