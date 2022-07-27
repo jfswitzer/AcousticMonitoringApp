bb518420b08118d5e3faf8cdf19c85a62cbc8192
juliale02 <juliale0212@gmail.com>
2022-07-27 11:29:24 -0700

    Updated Apps

--- AudioMoth2/src/home/home-page.ts
+++ AudioMoth2/src/home/home-page.ts
@@ -1,17 +1,17 @@
-import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
 import { Application, EventData, NavigatedData, Page } from '@nativescript/core'
+import { View } from "tns-core-modules/ui/core/view";
 import { Button } from 'tns-core-modules/ui/button'
 import { HomeViewModel } from './home-view-model'
+
 
 export function onNavigatingTo(args: NavigatedData) {
   const page = <Page>args.object
   page.bindingContext = new HomeViewModel()
 }
+
+//not working, does not appear in console 
 export function onButtonTap(args: EventData) {
-  const button = <Button><unknown>args.object;
-  console.log('Button tapped');
+  //const button = <View><unknown>args.object
+  console.info("Button tapped");
 }
-export function onTap(args: EventData) {
-  let button = args.object as unknown as Button;
-  console.log('Button tapped');
-}
+
